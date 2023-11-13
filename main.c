b2b5b6c2c041f2bc6dea786124d0f15a8299409f
Kurt Butor-Blamont <kbutor-b@1B6.42angouleme.fr>
2023-11-13 15:03:25 +0100

    test

--- main.c
+++ main.c
@@ -6,19 +6,16 @@
 /*   By: kbutor-b <kbutor-b@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/31 15:01:30 by kbutor-b          #+#    #+#             */
-/*   Updated: 2023/10/31 15:01:38 by kbutor-b         ###   ########.fr       */
+/*   Updated: 2023/11/09 00:22:14 by kbutor-b         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-
+#include "libft.h"
 #include <stdio.h>
 #include <bsd/string.h>
 
-char	*strnstr(const char *big, const char *little, size_t len);
-
-int	main(int argc, char** argv)
+int	main()
 {
-	(void) argc;
-	printf("mylib :%s\n", ft_strnstr(argv[1], argv[2], 15));
-	printf("lib c :%s", strnstr(argv[1], argv[2], 15));
+	//(void)argc;
+	printf("%s", ft_strnstr("aaabcabcd", "abcd", -1));
 }
